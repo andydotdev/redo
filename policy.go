02403d5186d6de5134a021531b5fb1db9f86ca0c
Andy Walker <andy@andy.dev>
2024-03-29 14:09:25 -0400

    doc: duplicate defaults for Policy godoc

--- policy.go
+++ policy.go
@@ -5,13 +5,17 @@ import "time"
 // Policy allows you to predefine all of the options for a retry run ahead of
 // time and set them using [WithPolicy]
 type Policy struct {
-	// Initial median delay -- see [InitialDelay]
+	// Initial median delay.
+	// Default: (1 * time.Second)
 	InitialDelay time.Duration
-	// Maximum delay allowed -- see [MaxDelay]
+	// Maximum delay allowed.
+	// Default: (20*time.Minutes >= InitialDelay)
 	MaxDelay time.Duration
-	// Maximum number of tries to attempt -- see [MaxTries]
+	// Maximum number of tries to attempt.
+	// Default: 10
 	MaxTries int
-	// Whether to retry the first time immdiaitely -- see [FirstFast]
+	// Whether to retry the first time immdiaitely.
+	// Default: false
 	FirstFast bool
 	// Halt allows you to set a function to check for fatal errors -- see [Halt]
 	Halt func(error) bool
